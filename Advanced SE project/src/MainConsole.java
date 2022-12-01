882403448b5e15bae71ba2d64b6aa31d0f78852b
TheNight1234 <109628423+TheNight1234@users.noreply.github.com>
2022-12-01 14:51:07 +0200

    UserClass

--- Advanced SE project/src/MainConsole.java
+++ Advanced SE project/src/MainConsole.java
@@ -2,8 +2,9 @@
 public class MainConsole {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
 	}
 
 }
---
+++ Advanced SE project/src/User.java
@@ -0,0 +1,39 @@
+
+public class User {
+	private String name;
+	private String password;
+	private String email;
+	private double balance;
+	
+	User(String n,String p,String e)
+	{
+		name = n;
+		password = p;
+		email = p;
+		balance = 0;
+	}
+	
+	boolean compare_name(String n) {
+		return name.equals(n);
+	}
+	
+	boolean compare_email(String e)
+	{
+		return email.equals(e);
+	}
+	
+	String get_name()
+	{
+		return name;
+	}
+	
+	String get_email()
+	{
+		return email;
+	}
+	double get_balance()
+	{
+		return balance;
+	}
+	
+}

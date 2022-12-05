afad9c8db55f4e49a0e58d08be6536dbb0e206d5
TheNight1234 <109628423+TheNight1234@users.noreply.github.com>
2022-12-05 18:05:40 +0200

    added function  to service control

--- Advanced SE project/src/ServiceControl.java
+++ Advanced SE project/src/ServiceControl.java
@@ -3,6 +3,25 @@ import java.util.ArrayList;
 public class ServiceControl {
 	private Service s ;
 	
+	
+	Service ChooseService()
+	{
+		SystemServices s1 = SystemServices.getInstance();
+		
+		//get list of enums of services type
+		ArrayList<String> l1 = s1.get_services();
+		
+		//let user choose the service
+		SearchForm f = new SearchForm ();
+		String name = f.start(l1, "Choose the Service you want");
+		
+		
+		if(name == null)
+			return null;
+		//get instance of the object with the chosen enum
+		 s = s1.get_service(name);
+		 return s;
+	}
 	ServiceProvider choose_SP()
 	{
 		
